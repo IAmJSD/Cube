625ca2cc1fe779049c64e03d5551b44239eef040
JakeMakesStuff <jake@gealer.email>
2018-02-12 19:34:07 +0000

    Made help nicer and not hit the 2,000 char limit (as long as you ar <200 chars/command)

--- plugins/help.py
+++ plugins/help.py
@@ -3,8 +3,6 @@ import discord
 
 async def help(app):
 
-    help_str = ""
-
     is_management = False
     for role in app.message.author.roles:
         if "managers" in role.name.lower() or "management" in role.name.lower():
@@ -24,36 +22,39 @@ async def help(app):
         cursor.close()
 
     prefix = app.get_prefix(app.message.server.id)
-
-    for cmd in app.discord_commands:
-        show = True
-        try:
-            if app.discord_commands[cmd].requires_staff and not is_staff:
-                show = False
-        except:
-            pass
-        try:
-            if app.discord_commands[cmd].requires_management and not is_management:
-                show = False
-        except:
-            pass
-        try:
-            if app.discord_commands[cmd].requires_bot_admin and not is_bot_admin:
-                show = False
-        except:
-            pass
-        if show:
-            try:
-                desc = app.discord_commands[cmd].description
-            except:
-                desc = "No description found."
-            help_str = help_str + prefix + cmd + " - " + desc + "\n"
-
-    embed=discord.Embed(title="{} Help".format(app.config["bot_name"]), description="```{}```".format(help_str))
-    embed.set_footer(text=app.premade_ver)
+    x = [discord.Embed(title="Cube Help:")]
+    f = 0
 
     try:
-        await app.whisper(embed=embed)
+        for cmd in app.discord_commands:
+            show = True
+            try:
+                if app.discord_commands[cmd].requires_staff and not is_staff:
+                    show = False
+            except:
+                pass
+            try:
+                if app.discord_commands[cmd].requires_management and not is_management:
+                    show = False
+            except:
+                pass
+            try:
+                if app.discord_commands[cmd].requires_bot_admin and not is_bot_admin:
+                    show = False
+            except:
+                pass
+            if show:
+                try:
+                    desc = app.discord_commands[cmd].description
+                except:
+                    desc = "No description found."
+                if f == 10:
+                    x.append(discord.Embed())
+                    f = 0
+                x[-1].add_field(name=prefix + cmd, value=desc, inline=False)
+                f = f + 1
+        for y in x:
+            await app.whisper(embed=y)
         await app.say(embed=discord.Embed(title="📬 Check DM's"))
     except:
         await app.say(embed=discord.Embed(title="Could not DM.", color=0xff0000))
