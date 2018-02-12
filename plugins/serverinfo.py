68a4f820c9dca119a9b4b27c5f2cef1edbd0d9a3
JakeMakesStuff <jake@gealer.email>
2018-02-12 21:04:44 +0000

    Made the embed not inline

--- plugins/serverinfo.py
+++ plugins/serverinfo.py
@@ -4,10 +4,10 @@ import discord
 async def serverinfo(app):
     embed=discord.Embed(title="Info About {}:".format(app.message.server.name))
     embed.set_thumbnail(url=app.message.server.icon_url)
-    embed.add_field(name="Server ID:", value=app.message.server.id, inline=True)
-    embed.add_field(name="Server Owner:", value=app.message.server.owner.mention, inline=True)
-    embed.add_field(name="Member Count:", value=str(len(app.message.server.members)), inline=True)
-    embed.add_field(name="Created:", value=str(app.message.server.created_at), inline=True)
+    embed.add_field(name="Server ID:", value=app.message.server.id, inline=False)
+    embed.add_field(name="Server Owner:", value=app.message.server.owner.mention, inline=False)
+    embed.add_field(name="Member Count:", value=str(len(app.message.server.members)), inline=False)
+    embed.add_field(name="Created:", value=str(app.message.server.created_at), inline=False)
     embed.set_footer(text=app.premade_ver)
     await app.say(embed=embed)
 # Gets info about the server.
