4fb270e3a37d8d0e10bff1bbd50f9023bc5c047a
Jake Gealer <jake@gealer.email>
2019-12-29 22:15:24 +0000

    fix a status bug

--- cube/events/ready.go
+++ cube/events/ready.go
@@ -5,11 +5,6 @@ import "github.com/bwmarrin/discordgo"
 func init() {
 	// The ready event handler.
 	Events = append(Events, func(s *discordgo.Session, event *discordgo.Ready) {
-		_ = s.UpdateStatusComplex(discordgo.UpdateStatusData{
-			IdleSince: nil,
-			Game:      nil,
-			AFK:       false,
-			Status:    "Cube | c!help for help with the bot",
-		})
+		_ = s.UpdateStatus(0, "Cube | c!help for help with the bot")
 	})
 }
