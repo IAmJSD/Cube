7eb4308a6a7a71085966234eec6033f9a7845024
Jake Gealer <jake@gealer.email>
2020-01-01 16:00:05 +0000

    change how this function works

--- cube/wallets/purge_guild_wallets.go
+++ cube/wallets/purge_guild_wallets.go
@@ -2,23 +2,16 @@ package wallets
 
 import (
 	"github.com/jakemakesstuff/Cube/cube/redis"
-	"sync"
 )
 
 // PurgeGuildWallets is used to purge all wallets belonging to a guild.
 func PurgeGuildWallets(GuildID string) {
 	Wallets := redis.Client.SMembers("w:" + GuildID).Val()
-	Locks := make([]*sync.Mutex, len(Wallets))
-	for i, v := range Wallets {
+	for _, v := range Wallets {
 		Key := "b:" + GuildID + ":" + v
-		Wallets[i] = Key
-		Locks[i] = getBalanceLock(Key)
-	}
-	for _, v := range Locks {
-		v.Lock()
-	}
-	redis.Client.Del(Wallets...)
-	for _, v := range Locks {
-		v.Unlock()
+		Lock := getBalanceLock(Key)
+		Lock.Lock()
+		redis.Client.Set(Key, "0", 0)
+		Lock.Unlock()
 	}
 }
