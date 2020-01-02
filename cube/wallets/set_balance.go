8925b687ff320db1b50749d9204f5806911da97d
Jake Gealer <jake@gealer.email>
2020-01-02 19:13:21 +0000

    Move this to its own variable for debugging

--- cube/wallets/set_balance.go
+++ cube/wallets/set_balance.go
@@ -9,6 +9,7 @@ import (
 func SetBalance(UserID string, GuildID string, Amount int64) {
 	Key, Lock := ensureWallet(GuildID, UserID)
 	Lock.Lock()
-	redis.Client.Set(Key, strconv.FormatInt(Amount, 10), 0)
+	Formatted := strconv.FormatInt(Amount, 10)
+	redis.Client.Set(Key, Formatted, 0)
 	Lock.Unlock()
 }
